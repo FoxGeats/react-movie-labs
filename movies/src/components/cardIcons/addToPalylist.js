847948b0d7092822c5169b4cab4687bb2b8695f9
Shu Chen <GGGGeats@gmail.com>
2022-11-03 16:33:23 +0000

    New button for upcoming movies page.

---
+++ movies/src/components/cardIcons/addToPalylist.js
@@ -0,0 +1,20 @@
+import React, { useContext } from "react";
+import { MoviesContext } from "../../contexts/moviesContext";
+import IconButton from "@mui/material/IconButton";
+import PlaylistAddIcon from "@mui/icons-material/PlaylistAdd";
+
+const AddToPlayListIcon = ({ movie }) => {
+  const context = useContext(MoviesContext);
+
+  const handleAddToPlayList = (e) => {
+    
+  };
+
+  return (
+    <IconButton aria-label="add to play list" onClick={handleAddToPlayList}>
+      <PlaylistAddIcon color="primary" fontSize="large" />
+    </IconButton>
+  );
+};
+
+export default AddToPlayListIcon;
